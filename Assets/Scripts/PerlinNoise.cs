ed2ed8f2b302fdb72858e0c374ad307df9c7a1c3
Alexis <alexis.morin.mail@gmail.com>
2020-07-28 19:43:27 -0400

    Create PerlinNoise.cs

---
+++ Assets/Scripts/PerlinNoise.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Perlin Noise", menuName = "Perlin Noise")]
+public class PerlinNoise : ScriptableObject {
+
+    public float size = 0.01f;
+    public float strength = 1f;
+    public Vector2 offset = Vector2.zero;
+    public AnimationCurve falloffRemapCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+
+    public float ReturnNoise(Vector3 samplePos) {
+        return falloffRemapCurve.Evaluate(Mathf.PerlinNoise((samplePos.x + offset.x) * size, (samplePos.z + offset.y) * size)) * strength;
+    }
+
+}
