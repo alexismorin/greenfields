424238c147e2b194d9ed90f73190de0ef2b711a5
Alexis Morin <alexis.morin.mail@gmail.com>
2020-07-29 17:21:55 -0400

    behaviour tooltips

--- Assets/Scripts/AI/Behaviour.cs
+++ Assets/Scripts/AI/Behaviour.cs
@@ -3,14 +3,16 @@
 [CreateAssetMenu (fileName = "AI Behaviour", menuName = "AI Behaviour", order = 1)]
 public class SpawnManagerScriptableObject : ScriptableObject {
 
-    [SerializeField]
-    bool wander = false;
-
     [Header ("Loop Behaviour")]
 
     [SerializeField]
+    float behaviourTime = 5f;
+
+    [Tooltip ("Searches the nearest object of this tag and walks toward it. If set to wander the character will do as such and use the satisfation distance as a range")]
+    [SerializeField]
     string seekNearest = null;
 
+    [Tooltip ("How close to the target does one have to be to acheive the current goal. ")]
     [SerializeField]
     float satisfactionDistance = 2f;
 
